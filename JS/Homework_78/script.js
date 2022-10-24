f99506743d9eed81d82fe6df33437d0f5e0e5878
Roman-cas88 <orekhovskiy.roman@gmail.com>
2022-10-24 21:55:37 +0200

    HomeWorkReact

--- JS/Homework_78/script.js
+++ JS/Homework_78/script.js
@@ -36,6 +36,7 @@
         setTimeout(view, 2000)
     }
 
+
     function view() {
         let url = 'https://file.io/';
         fetch(url, {
@@ -56,10 +57,18 @@
                 };
                 clearBox();
 
-                // View info whith download  
-                for (let i = 0; i < arr.length; i++) {
-                    document.getElementById('p').innerHTML += `File name: ${arr[i].name} <a href="${arr[i].link}" target="blank"> Download </a><br> 
-                    File key: ${arr[i].key}; Auto deleted at: ${arr[i].expires.slice(0, 10)}<br><br>`;
+                // function funcInfo({name,key}) {
+
+                //     document.getElementById('p').innerHTML = `File name: ${name}, file key: ${key}`
+                // }
+
+                // funcInfo(arr)
+
+                // View info whith download and delete  
+                for (var i = 0; i < arr.length; i++) {
+                    let key = arr[i].key
+                    document.getElementById('p').innerHTML += `File name: <a href="${arr[i].link}" target="blank">${arr[i].name}</a><br> 
+                                File key: ${key}; Auto deleted at: ${arr[i].expires.slice(0, 10)}<button class="download" onclick="">Delete</button><br><br>`;
                 }
             })
             .catch((error) => {
@@ -67,48 +76,68 @@
                 alert('Error:', error);
             });
 
-    }
+    };
+
 
     function put() {
 
-var file = document.getElementById('file')
+        var file = document.getElementById('file')
 
-// set date from today + 5 days
-let date = new Date();
-date.setDate(date.getDate() + 5)
-let expires = date.toISOString().slice(0, 10)
+        // set date from today + 5 days
+        let date = new Date();
+        date.setDate(date.getDate() + 5)
+        let expires = date.toISOString().slice(0, 10)
 
-var data = new FormData()
-data.append('file', file.files[0])
-data.append('expires', expires)
-data.append('maxDownloads', 1)
-data.append('autoDelete', true)
+        var data = new FormData()
+        data.append('file', file.files[0])
+        data.append('expires', expires)
+        data.append('maxDownloads', 1)
+        data.append('autoDelete', true)
 
-let fileId = document.getElementById('fileId').value;
+        let fileId = document.getElementById('fileId').value;
 
-let url = new URL(fileId, 'https://file.io/');
-console.log(url)
-fetch(url, {
-        method: "PUT",
-        headers: {
-            'Authorization': 'Bearer F3WBRZN.F8WSGY3-CFM47NE-PY0BSTC-67XDRJJ',
-        },
-        body: data
-    })
+        let url = new URL(fileId, 'https://file.io/');
+        console.log(url)
+        fetch(url, {
+                method: "PUT",
+                headers: {
+                    'Authorization': 'Bearer F3WBRZN.F8WSGY3-CFM47NE-PY0BSTC-67XDRJJ',
+                },
+                body: data
+            })
 
-    .then((response) => response.json())
-    .then((data) => {
-        alert("File change successfully")
-        console.log('Success:', data);
-    })
-    .catch((error) => {
-        console.log('Error:', error);
-    });
+            .then((response) => response.json())
+            .then((data) => {
+                alert("File change successfully")
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.log('Error:', error);
+            });
 
-// Add new file to list vith timeout
-setTimeout(view, 2000)
-}
+        // Add new file to list vith timeout
+        setTimeout(view, 2000)
+    }
 
-    
-        // show list of files
+
+    // show list of files
     view()
+
+
+    // function del() {
+    //     fetch(`https://file.io/${key}`, {
+    //         method: "DELETE",
+    //         headers: {
+    //             'Authorization': 'Bearer F3WBRZN.F8WSGY3-CFM47NE-PY0BSTC-67XDRJJ',
+    //         }
+    //     })
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //         alert("File deleted successfully")
+    //         console.log('Success:', data);
+    //     })
+    //     .catch((error) => {
+    //         console.log('Error:', error);
+    //     });
+    //     view()
+    // }
